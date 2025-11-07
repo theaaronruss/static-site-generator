e0d8ca56e705718c77bb28e1b78dcf6091960b4b
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-11-07 16:04:24 -0500

    add leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,17 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("leaf node must have value")
+
+        if self.tag is None:
+            return self.value
+
+        props = self.props_to_html()
+        return f"<{self.tag}{props}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html(self):
@@ -18,3 +19,24 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_none(self):
         node = HTMLNode("a", "link", [HTMLNode(), HTMLNode()], {})
         self.assertEqual("", node.props_to_html())
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_raw_value(self):
+        node = LeafNode(None, "The value", None)
+        self.assertEqual("The value", node.to_html())
+
+    def test_to_html_no_value(self):
+        node = LeafNode("a", None, None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_to_html(self):
+        node = LeafNode("a", "click", {"href": "https://www.boot.dev", "target": "_blank"})
+        self.assertEqual('<a href="https://www.boot.dev" target="_blank">click</a>', node.to_html())
+
+    def test_to_html_no_props(self):
+        node = LeafNode("a", "click", None)
+        self.assertEqual("<a>click</a>", node.to_html())
+        
+    def test_to_html_empty_props(self):
+        node = LeafNode("a", "click", {})
+        self.assertEqual("<a>click</a>", node.to_html())
